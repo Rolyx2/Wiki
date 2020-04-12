bc2a6e4d0e52338bf01ad0000f0be687d520a335
Rolyx_2 <rolyx21@gmail.com>
2020-04-12 20:55:23 +0500

    228

---
+++ precache-manifest.042d2f5d23773092892f69d697871370.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee042ac5b51e7e9c9d97a49f8aced2a7",
+    "url": "/Wiki/index.html"
+  },
+  {
+    "revision": "3bdb101f729d1a630b2f",
+    "url": "/Wiki/static/css/main.0b0d0c88.chunk.css"
+  },
+  {
+    "revision": "d3e4aaae9fcde82d216d",
+    "url": "/Wiki/static/js/2.c9a9385e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Wiki/static/js/2.c9a9385e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bdb101f729d1a630b2f",
+    "url": "/Wiki/static/js/main.81195794.chunk.js"
+  },
+  {
+    "revision": "43394afa12d80e00c601",
+    "url": "/Wiki/static/js/runtime-main.674ac7ec.js"
+  }
+]);
